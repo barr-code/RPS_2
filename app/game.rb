269c03d44c54e939d369d4c6c7fada8807281e18
Victoria Barr <vb329@georgetown.edu>
2014-10-18 21:54:47 +0100

    Game functionality in place; time to build it into the web app

--- app/game.rb
+++ app/game.rb
@@ -1,8 +1,28 @@
+require_relative 'player'
+
 class Game
 
-	attr_accessor :player
+	attr_accessor :player, :computer_choice, :winner
 
 	def initialize
 		@player = nil
+		@computer_choice = nil
+		@winner = nil
 	end
+
+	DEFEATS = {"rock" => "scissors",
+		"scissors" => "paper",
+		"paper" => "rock"}
+
+	def random_choice
+		@computer_choice = %w(rock paper scissors).sample
+	end
+
+	def result
+		random_choice
+		@winner = 'draw' if @player.weapon == @computer_choice
+		@winner = @player if DEFEATS[@player.weapon] == @computer_choice
+		@winner = @computer_choice if DEFEATS[@computer_choice] == @player.weapon
+	end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require './app/game'
 describe Game do
 
 	let(:game) {Game.new}
-	let(:player) {double :player, :name => "Victoria"}
+	let(:player) {Player.new("Victoria")}
 
 	it "starts out without a player" do
 		expect(game.player).to eq nil
@@ -12,6 +12,26 @@ describe Game do
 	it "players join with names" do
 		game.player = player
 		expect(game.player.name).to eq "Victoria"
+	end
+
+	it "starts without a computer choice" do
+		expect(game.computer_choice).to eq nil
+	end
+
+	it "sets a computer choice randomly" do
+		game.random_choice
+		expect(game.computer_choice).not_to eq nil
+	end
+
+	it "starts out without a winner" do
+		expect(game.winner).to eq nil
+	end
+
+	it "knows who wins the game" do
+		game.player = player
+		game.player.weapon = "scissors"
+		game.result
+		expect(game.winner).not_to eq nil
 	end
 
 end
